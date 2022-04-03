8e4b9805ea55784ceb351314df7cc2c62fed1dee
Alex Mwangi <alex@stl-horizon.com>
2022-04-03 22:10:39 +0300

    Added users as public property;Fetched all the users from users table

--- app/Http/Livewire/User.php
+++ app/Http/Livewire/User.php
@@ -3,15 +3,15 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\User as Users;
 
 class User extends Component
 {
+    public $users;
+
     public function render()
     {
-        return <<<'blade'
-            <div>
-              <h1 class="text-center">This is user component</h1>
-            </div>
-        blade;
+        $this->users = Users::all();
+        return view('livewire.user');
     }
 }
