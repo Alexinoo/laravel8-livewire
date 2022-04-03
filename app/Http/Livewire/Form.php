4c0d63c4b840d98438ae97dc484657d1c111d669
Alex Mwangi <alex@stl-horizon.com>
2022-04-03 12:43:01 +0300

    Defined various variables for textarea,radio,checkbo and select textfields

--- app/Http/Livewire/Form.php
+++ app/Http/Livewire/Form.php
@@ -7,6 +7,10 @@ use Livewire\Component;
 class Form extends Component
 {
     public $name;
+    public $message;
+    public $marital_status;
+    public $colors = [];
+    public $fruit;
 
     public function render()
     {
