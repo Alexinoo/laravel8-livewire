92bc95172e447faaaa5f6121004cbdf78da3377d
Alex Mwangi <alex@stl-horizon.com>
2022-04-03 14:27:18 +0300

    Hooked bootstrap css/jss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LiveWire Project</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css')}}">
     @livewireStyles
 </head>
 <body>
     
     {{ $slot }}
 
+    <script src="{{asset('assets/js/jquery-3.6.0.min.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
     @livewireScripts
 </body>
 </html>
