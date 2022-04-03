d86a94b44c9e221c5fd4f951271167a468cf18ec
Alex Mwangi <alex@stl-horizon.com>
2022-04-03 12:14:03 +0300

    Added route for rendering form view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Form;
 use App\Http\Livewire\Home;
 use App\Http\Livewire\Post;
 use App\Http\Livewire\User;
@@ -29,3 +30,7 @@ Route::get('home/{name}', Home::class);
 
 // Pass optional parameters
 // Route::get('home/{name?}', Home::class);
+
+
+// PROPERTY BINDING
+Route::get('form', Form::class);
