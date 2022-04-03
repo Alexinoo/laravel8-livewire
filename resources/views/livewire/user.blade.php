a8a72c555ac64b2003974ef89ca325535bd77d90
Alex Mwangi <alex@stl-horizon.com>
2022-04-03 22:11:11 +0300

    Added users view - display in a table

---
+++ resources/views/livewire/user.blade.php
@@ -0,0 +1,35 @@
+<div>
+ <section>
+     <div class="container">
+         <div class="row">
+             <div class="col-md-12">
+                 <div class="card mt-3">
+                     <div class="card-header text-center">
+                         <h4>All Users</h4>
+                     </div>
+                     <div class="card-body">
+                         <table class="table table-striped">
+                             <thead>
+                                 <tr>
+                                     <th>ID</th>
+                                     <th>Name</th>
+                                     <th>Email</th>
+                                 </tr>
+                             </thead>
+                             <tbody>
+                                 @foreach($users as $key => $user)
+                                  <tr>
+                                    <td>{{ $user->id}}</td>      
+                                    <td>{{ $user->name}}</td>      
+                                    <td>{{ $user->email}}</td>      
+                                </tr>   
+                                 @endforeach
+                             </tbody>
+                         </table>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
+</div>
