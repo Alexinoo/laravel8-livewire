4690ba1b0786edc421492e7014f324718933d27e
Alex Mwangi <alex@stl-horizon.com>
2022-04-05 22:04:17 +0300

    Added edit(id),update() and delete() functionality

--- app/Http/Livewire/Students.php
+++ app/Http/Livewire/Students.php
@@ -7,6 +7,7 @@ use Livewire\Component;
 
 class Students extends Component
 {
+    public $ids;
     public $firstname;
     public $lastname;
     public $email;
@@ -31,6 +32,53 @@ class Students extends Component
         session()->flash('message', 'Student Added successfully');
         $this->resetInputFields();
         $this->emit('StudentAdded');
+    }
+    public function edit($id)
+    {
+        $student = Student::where('id', $id)->first();
+
+        // populate the input fields
+        $this->ids = $student->id;
+        $this->firstname = $student->firstname;
+        $this->lastname = $student->lastname;
+        $this->email = $student->email;
+        $this->phone = $student->phone;
+    }
+    public function update()
+    {
+        //Validate all inputs required
+        $this->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required'
+        ]);
+
+
+        if ($this->ids) {
+            $student = Student::find($this->ids);
+            $student->update([
+                'firstname' => $this->firstname,
+                'lastname' => $this->lastname,
+                'email' => $this->email,
+                'phone' => $this->phone,
+            ]);
+
+            // Show update flash
+            session()->flash('message', 'Student Updated successfully');
+            $this->resetInputFields();
+            $this->emit('StudentUpdated');
+        }
+    }
+
+    public function delete($id)
+    {
+
+        $student = Student::find($id);
+        $student->delete();
+
+        // Show delete flash
+        session()->flash('message', 'Student Deleted successfully');
     }
 
     public function render()
