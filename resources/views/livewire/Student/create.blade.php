c62de67b2eb42a0f4f3caae809f265418c8a66a1
Alex Mwangi <alex@stl-horizon.com>
2022-04-05 22:07:14 +0300

    Changed id of the modal to addStudentModal

--- resources/views/livewire/Student/create.blade.php
+++ resources/views/livewire/Student/create.blade.php
@@ -1,5 +1,5 @@
 <!-- Modal -->
-<div class="modal fade" id="studentModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" wire:ignore.self>
+<div class="modal fade" id="addStudentModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" wire:ignore.self>
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -10,19 +10,32 @@
           <div class="form-group mb-3">
             <label for="">First name : </label>
             <input type="text" name="firstname" id="" class="form-control" wire:model = "firstname"/>
+               @error('firstname')
+                <span class="text-danger">{{$message}}</span>
+            @enderror
           </div>
           <div class="form-group mb-3">
             <label for="">Last name : </label>
             <input type="text" name="lastname" id="" class="form-control" wire:model = "lastname"/>
+               @error('lastname')
+                <span class="text-danger">{{$message}}</span>
+            @enderror
           </div>
           <div class="form-group mb-3">
             <label for="">Email : </label>
             <input type="email" name="email" id="" class="form-control" wire:model = "email"/>
+               @error('email')
+                <span class="text-danger">{{$message}}</span>
+            @enderror
           </div>
           <div class="form-group mb-3">
             <label for="">Phone : </label>
             <input type="text" name="phone" id="" class="form-control" wire:model = "phone"/>
+               @error('phone')
+                <span class="text-danger">{{$message}}</span>
+            @enderror
           </div>
+          
       </form>
       </div>
       <div class="modal-footer">
