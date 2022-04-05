abf51b2a37ab56bbcf3247450ad6375a1bb5e5f9
Alex Mwangi <alex@stl-horizon.com>
2022-04-05 21:18:21 +0300

    Created Student lieviwre class - Added validations and store() to store student details

---
+++ app/Http/Livewire/Students.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Student;
+use Livewire\Component;
+
+class Students extends Component
+{
+    public $firstname;
+    public $lastname;
+    public $email;
+    public $phone;
+
+    public function resetInputFields()
+    {
+        $this->firstname = '';
+        $this->lastname = '';
+        $this->email = '';
+        $this->phone = '';
+    }
+    public function store()
+    {
+        $validatedData = $this->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required'
+        ]);
+        Student::create($validatedData);
+        session()->flash('message', 'Student Added successfully');
+        $this->resetInputFields();
+        $this->emit('StudentAdded');
+    }
+
+    public function render()
+    {
+        $students = Student::orderBy('id', 'DESC')->get();
+
+        return view('livewire.Student.students', compact('students'));
+    }
+}
